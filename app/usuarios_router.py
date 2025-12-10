55ed1e4e832c42cf76c1e9228fc4e43c98293748
johergo2 <johergo2@gmail.com>
2025-12-10 13:48:58 -0500

    Ajuste en usuarios_router.py para validar que el usuario esté activo

--- app/usuarios_router.py
+++ app/usuarios_router.py
@@ -27,12 +27,12 @@ async def listar_usuarios(db: AsyncSession = Depends(get_db)):
 # =======================================================
 @router.get("/usuarios/{id}")
 async def obtener_usuario(id: int, db: AsyncSession = Depends(get_db)):
-    query = text("SELECT * FROM usuarios WHERE id = :id")
+    query = text("""SELECT * FROM usuarios WHERE id = :id AND estado = 'ACT'""" )
     result = await db.execute(query, {"id": id})
     row = result.mappings().first()
 
     if not row:
-        raise HTTPException(status_code=404, detail="Usuario con id no encontrado")
+        raise HTTPException(status_code=404, detail="Usuario con id no encontrado o inactivo")
 
     return row
 
@@ -42,14 +42,14 @@ async def obtener_usuario(id: int, db: AsyncSession = Depends(get_db)):
 # =======================================================
 @router.get("/usuarios/nombre/{nombre}")
 async def obtener_usuario_por_nombre(nombre: str, db: AsyncSession = Depends(get_db)):
-    query = text("SELECT * FROM usuarios WHERE nombre = :nombre")
+    query = text("""SELECT * FROM usuarios WHERE nombre = :nombre AND estado = 'ACT'""")
     result = await db.execute(query, {"nombre": nombre})
     row = result.mappings().first()
 
     if not row:
         raise HTTPException(
             status_code=404,
-            detail=f"No existe un usuario con el nombre '{nombre}'",
+            detail=f"No existe un usuario activo con el nombre '{nombre}'",
         )
 
     return row
@@ -69,12 +69,12 @@ async def login(datos: dict, db: AsyncSession = Depends(get_db)):
             detail="Nombre y contraseña son obligatorios"
         )
 
-    query = text("SELECT * FROM usuarios WHERE nombre = :nombre")
+    query = text("""SELECT * FROM usuarios WHERE nombre = :nombre AND estado = 'ACT'""")
     result = await db.execute(query, {"nombre": nombre})
     usuario = result.mappings().first()
 
     if not usuario:
-        raise HTTPException(status_code=404, detail="El usuario no existe")
+        raise HTTPException(status_code=404, detail="El usuario no existe o está inactivo")
 
     if usuario["contrasena"] != contrasena:
         raise HTTPException(status_code=401, detail="Contraseña incorrecta")
