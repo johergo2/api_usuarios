b7562d492ec7b20bb0bb281c1a23aab19764681f
johergo2 <johergo2@gmail.com>
2025-12-16 12:25:56 -0500

    Opción para la creación de Categorias (pantalla)

--- app/categorias_router.py
+++ app/categorias_router.py
@@ -62,3 +62,65 @@ async def listar_categorias_evento(
         "evento_id": evento_id,
         "categorias": categorias
     }
+
+# ============================================
+# 3. Crear categoria
+# ============================================
+@router.post("/categorias")
+async def crear_categoria(categoria: dict, db: AsyncSession = Depends(get_db)):
+
+    print("📌 Datos recibidos en crear_categoria:", categoria)
+    
+    query = text("""
+        INSERT INTO categorias (categoria)
+        VALUES (:categoria)
+        RETURNING *;
+    """)
+
+    result = await db.execute(query, categoria)
+    nueva_categoria = result.mappings().first()
+    await db.commit()
+
+    return nueva_categoria
+
+# ============================================
+# 4. Actualizar categoria
+# ============================================
+@router.put("/categoria/{id}")
+async def actualizar_categoria(id: int, datos: dict, db: AsyncSession = Depends(get_db)):
+
+    datos["id"] = id
+
+    query = text("""
+        UPDATE categorias
+        SET categoria = :categoria,                        
+            fecha_actualizacion = CURRENT_TIMESTAMP
+        WHERE id = :id
+        RETURNING *;
+    """)
+
+  
+    result = await db.execute(query, datos)
+    categoria = result.mappings().first()
+
+    if not categoria:
+        raise HTTPException(status_code=404, detail="Categoria no encontrada")
+
+    await db.commit()
+    return categoria
+
+
+# ============================================
+# 5. Eliminar categoria
+# ============================================
+@router.delete("/categoria/{id}")
+async def eliminar_categoria(id: int, db: AsyncSession = Depends(get_db)):
+    query = text("DELETE FROM categorias WHERE id = :id RETURNING id")
+    result = await db.execute(query, {"id": id})
+    categoria = result.mappings().first()
+
+    if not categoria:
+        raise HTTPException(status_code=404, detail="Categoria no encontrada")
+
+    await db.commit()
+    return {"message": "categoria eliminada correctamente", "id": id}
